4cbd53b22174a7c8083bbb7650af9d7c664f39f8
Rafael Bento <rafaelhgsbento@gmail.com>
2020-09-07 17:06:19 -0300

    Remove let dos testes

--- spec/services/user_create_service_spec.rb
+++ spec/services/user_create_service_spec.rb
@@ -1,25 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UserCreateService do
-  subject do
-    described_class.new(**user_params)
-  end
-
-  let(:user_params) do
-    {
-      full_name: 'Fausto Silva',
-      document: '12345678910',
-      address: 'Rua da Minha Casa, 1000',
-      birthday: '02/05/1950',
-      gender: 'M',
-      password: 'olocomeu'
-    }
-  end
-
   describe '#create' do
-    let(:user) { subject.create }
-
     it 'cria um usuário com os dados informados' do
+      user_params = {
+        full_name: 'Fausto Silva', document: '12345678910',
+        address: 'Rua da Minha Casa, 1000', birthday: '02/05/1950', gender: 'M',
+        password: 'olocomeu'
+      }
+
+      user = described_class.new(**user_params).create
+
       expect(user).to be_persisted
       expect(user.full_name).to eq('Fausto Silva')
       expect(user.document).to eq('12345678910')
@@ -32,22 +23,26 @@ RSpec.describe UserCreateService do
     it 'gera um token usando o SecureRandom' do
       allow(SecureRandom).to receive(:hex).and_return('DuMMyToKeN')
 
+      user_params = {
+        full_name: 'Fausto Silva', document: '12345678910',
+        address: 'Rua da Minha Casa, 1000', birthday: '02/05/1950', gender: 'M',
+        password: 'olocomeu'
+      }
+
+      user = described_class.new(**user_params).create
+
       expect(user.token).to eq('DuMMyToKeN')
     end
 
     context 'quando há campos obrigatórios não preenchidos ou mínimo de dígitos não atendido' do
-      let(:user_params) do
-        {
-          full_name: 'Fausto Silva',
-          document: '123',
-          address: '',
-          birthday: '',
-          gender: 'M',
-          password: 'errou'
-        }
-      end
-
       it 'não cria conta e retorna usuário não persistido e seus respectivos erros' do
+        user_params = {
+          full_name: 'Fausto Silva', document: '123', address: '', birthday: '',
+          gender: 'M', password: 'errou'
+        }
+
+        user = described_class.new(**user_params).create
+
         expect(user.account).to be_nil
         expect(user).not_to be_persisted
         expect(user.errors.full_messages).to include('Document is the wrong' \
@@ -64,6 +59,11 @@ RSpec.describe UserCreateService do
     it 'inicia o serviço, invoca e retorna o resultado de #create' do
       service = instance_double('UserCreateService')
       user = instance_double('User')
+      user_params = {
+        full_name: 'Fausto Silva', document: '12345678910',
+        address: 'Rua da Minha Casa, 1000', birthday: '02/05/1950', gender: 'M',
+        password: 'olocomeu'
+      }
 
       expect(described_class).to receive(:new).with(user_params).once.and_return(service)
       expect(service).to receive(:create).and_return(user)
