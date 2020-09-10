a95fc6e91d294944ccc48a0f915a59f2c51edea3
Rafael Bento <rafaelhgsbento@gmail.com>
2020-09-10 08:53:34 -0300

    Separa saques em um controller próprio
    O AccountsController estava com muitas responsabilidades e muito grande.
    
    Achei mais prudente dividir essa responsabilidade com um controller
    próprio para os saques.
    
    Pretendo, ainda, adicionar as outras operações em controllers próprios.

---
+++ app/controllers/api/v1/withdraws_controller.rb
@@ -0,0 +1,89 @@
+module Api
+  module V1
+    class WithdrawsController < ApplicationController
+      def withdraw
+        withdraw_request = AccountWithdrawService.new(
+          token: request.headers['Authorization'],
+          amount: withdraw_params[:amount]
+        ).withdraw
+
+        if withdraw_request
+          cash_possibilities = withdraw_request.cash_possibilities.split('|')
+          possibilities = normalized_possibilities(cash_possibilities)
+
+          render(
+            json: {
+              message: 'Responda "1" para a opção 1 e "2" para a opção 2',
+              id: withdraw_request.id,
+              amount: withdraw_request.amount.to_i,
+              cash_possibilities: possibilities
+            },
+            status: :created
+          )
+        else
+          render json: { message: 'Requisição de saque não autorizada!' },
+                 status: :forbidden
+        end
+      end
+
+      def confirm
+        withdraw_id = withdraw_confirm_params[:account_withdraw_request_id]
+        account_withdraw_request = AccountWithdrawRequest.where(id: withdraw_id).last
+
+        if account_withdraw_request.nil?
+          return render(json: { message: 'Requisição de saque não encontrada!' },
+                        status: :not_found)
+        end
+
+        withdraw_transaction = WithdrawConfirmService.new(
+          token: request.headers['Authorization'],
+          account_withdraw_request: account_withdraw_request,
+          possibility: withdraw_confirm_params[:possibility]
+        ).confirm
+
+        if withdraw_transaction
+          render(
+            json: {
+              message: 'Saque realizado com sucesso!',
+              amount: withdraw_transaction.amount.to_i,
+              balance: withdraw_transaction.account.balance.to_f
+            },
+            status: :created
+          )
+        else
+          render json: { message: 'Saque não autorizado!' },
+                 status: :forbidden
+        end
+      end
+
+      private
+
+      def normalized_possibilities(cash_possibilities)
+        possibilities = []
+
+        cash_possibilities.each do |possibility|
+          temp_array = []
+
+          possibility.split('-').each do |quantity_money|
+            quantity = quantity_money.split('x').first
+            money = quantity_money.split('x').last
+
+            temp_array << "#{quantity} nota(s) de #{money}"
+          end
+
+          possibilities << temp_array.join(', ')
+        end
+
+        possibilities
+      end
+
+      def withdraw_params
+        params.permit(:amount)
+      end
+
+      def withdraw_confirm_params
+        params.permit(:account_withdraw_request_id, :possibility)
+      end
+    end
+  end
+end
